581734268308b0ada999b5e8bd07260d45fc8f52
nvaytet <neil.vaytet@nbi.ku.dk>
2017-03-27 11:21:47 +0200

    modified the demo

--- demo.py
+++ demo.py
@@ -3,14 +3,8 @@ import numpy as np
 import osiris as pp
 import sys
 
-# Read arguments
-if len(sys.argv) > 1:
-    nout = int(sys.argv[1])
-else:
-    nout = 1
-
 # Load data
-mydata = pp.RamsesData(nout=nout,center="auto",scale="au")
+mydata = pp.RamsesData(nout=71,center="auto",scale="au")
 
 # Create figure
 fig = plt.figure()
@@ -26,18 +20,25 @@ ax6 = fig.add_subplot(236)
 
 dx = 100
 
-# Density vs B field
+# Density vs B field with AMR level contours
 mydata.plot_histogram("log_rho","log_B",var_z="level",axes=ax1,cmap="YlGnBu")
-# Density vs Temperature
-mydata.plot_histogram("log_rho","log_T",axes=ax2,cmap="YlGnBu")
 
-#x,z density slice with B field
+# Create new field
+mydata.new_field(name="log_vel",operation="np.log10(np.sqrt(velocity_x**2+velocity_y**2+velocity_z**2))",unit="cm/s",label="log(Velocity)")
+
+# Density vs log_vel
+mydata.plot_histogram("log_rho","log_vel",axes=ax2,cmap="YlGnBu")
+
+#x,z density slice with B field streamlines
 mydata.plot_slice("log_rho",direction="y",stream="B",dx=dx,axes=ax3)
-# x,y density slice with velocity
+# x,y density slice with velocity vectors in color
 mydata.plot_slice("log_rho",direction="z",vec="velocity",dx=dx,axes=ax4,vskip=4,vcmap="Greys")
-# x,y temperature slice with velocity
+# x,y temperature slice with velocity vectors
 mydata.plot_slice("log_T",direction="z",vec="velocity",dx=dx,axes=ax5,cmap="hot")
-# x,z density slice with velocity
-mydata.plot_slice("log_rho",direction="y",vec="velocity",dx=dx*3,axes=ax6)
 
-fig.savefig("plots.pdf",bbox_inches="tight")
+# Now update values with later snapshot
+mydata.update_values(201)
+# Re-plot x,y density slice with velocity vectors in color
+mydata.plot_slice("log_rho",direction="z",vec="velocity",dx=100,axes=ax6)
+
+fig.savefig("demo.pdf",bbox_inches="tight")
