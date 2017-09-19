be3e905e50488be34fc9075348ff5d6f4fe7e42e
nvaytet <neil.vaytet@nbi.ku.dk>
2017-09-19 14:59:42 +0200

    Added GPLv3 licence

--- osiris.py
+++ osiris.py
@@ -1,3 +1,20 @@
+#=======================================================================================
+#This file is part of OSIRIS.
+
+#OSIRIS is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#OSIRIS is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with OSIRIS.  If not, see <http://www.gnu.org/licenses/>.
+#=======================================================================================
+
 import config_osiris as conf
 import load_ramses_data
 import numpy as np
