02b2c840bcba6280d2323dbc345b1c12647c0873
nvaytet <neil.vaytet@nbi.ku.dk>
2018-01-08 19:21:06 +0100

    added opacity table in ism_physics

--- ism_physics.py
+++ ism_physics.py
@@ -21,9 +21,10 @@ import struct
 from scipy.interpolate import RegularGridInterpolator
 
 #===================================================================================
-# An empty EOS table class which will be filled up as the EOS file is read
+# An empty container class which will be filled up as the EOS, opacity, resistivity
+# tables are read.
 #===================================================================================
-class EosTable():
+class IsmTable():
     
     def __init__(self):
                 
@@ -45,19 +46,19 @@ def read_eos_table(fname="tab_eos.dat"):
     data_fields = ["rho_eos","ener_eos","temp_eos","pres_eos","s_eos","cs_eos","xH_eos","xH2_eos","xHe_eos","xHep_eos"]
 
     # Create table container
-    theTable = EosTable()
+    theTable = IsmTable()
     
     # Initialise offset counters and start reading data
     ninteg = nfloat = nlines = nstrin = nquadr = 0
     
     # Get table dimensions
-    [theTable.nRho,theTable.nEner] = get_binary_data(fmt="2i",content=eosContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat,nstrin=nstrin,nquadr=nquadr)
+    [theTable.nRho,theTable.nEner] = get_binary_data(fmt="2i",content=eosContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
     
     # Get table limits
     ninteg += 2
     nlines += 1
     [theTable.rhomin,theTable.rhomax,theTable.emin,theTable.emax,theTable.yHe] = \
-        get_binary_data(fmt="5d",content=eosContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat,nstrin=nstrin,nquadr=nquadr)
+        get_binary_data(fmt="5d",content=eosContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
         
     array_size = theTable.nRho*theTable.nEner
     array_fmt  = "%id" % array_size
@@ -67,7 +68,7 @@ def read_eos_table(fname="tab_eos.dat"):
     # Now loop through all the data fields
     for i in range(len(data_fields)):
         setattr(theTable,data_fields[i],np.reshape(get_binary_data(fmt=array_fmt,content=eosContent, \
-                ninteg=ninteg,nlines=nlines,nfloat=nfloat,nstrin=nstrin,nquadr=nquadr),[theTable.nRho,theTable.nEner],order="F"))
+                ninteg=ninteg,nlines=nlines,nfloat=nfloat),[theTable.nRho,theTable.nEner],order="F"))
         nfloat += array_size
         nlines += 1
     
@@ -79,7 +80,7 @@ def read_eos_table(fname="tab_eos.dat"):
 
 
 #===================================================================================
-# Function to read in binary file containing EOS table
+# Function to interpolate simulation data onto EOS table
 #===================================================================================
 def get_eos_variables(holder,eos_fname="tab_eos.dat",variables=["temp_eos","pres_eos","s_eos","cs_eos","xH_eos","xH2_eos","xHe_eos","xHep_eos"]):
     
@@ -105,18 +106,106 @@ def get_eos_variables(holder,eos_fname="tab_eos.dat",variables=["temp_eos","pres
 
 
 
+
+
 #===================================================================================
 #===================================================================================
 #===================================================================================
 
 #===================================================================================
-# An empty table class which will be filled up as the resistivity file is read
+# Function to read in binary file containing opacity table
 #===================================================================================
-class ResistivityTable():
+def read_opacity_table(fname="vaytet_grey_opacities3D.bin"):
     
-    def __init__(self):
-                
-        return
+    print("Loading opacity table: "+fname)
+    
+    # Read binary resistivity file
+    with open(fname, mode='rb') as kappa_file:
+        kappaContent = kappa_file.read()
+    kappa_file.close()
+    
+    # Create table container
+    theTable = IsmTable()
+    
+    # Initialise offset counters and start reading data
+    ninteg = nfloat = nlines = nstrin = nquadr = 0
+    
+    # Get table dimensions
+    [theTable.nx,theTable.ny,theTable.nz] = get_binary_data(fmt="3i",content=kappaContent)
+    
+    # Get table limits
+    [theTable.dx,theTable.dy,theTable.dz,theTable.xmin,theTable.xmax,theTable.ymin,theTable.ymax,theTable.zmin,theTable.zmax] = \
+        get_binary_data(fmt="9d",content=kappaContent,correction=12)
+    
+    # Read table coordinates:
+    
+    # x: density
+    ninteg += 3
+    nfloat += 9
+    nlines += 1
+    theTable.dens = get_binary_data(fmt="%id"%theTable.nx,content=kappaContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+    
+    # y: gas temperature
+    nfloat += theTable.nx
+    nlines += 1
+    theTable.tgas = get_binary_data(fmt="%id"%theTable.ny,content=kappaContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+    
+    # z: radiation temperature
+    nfloat += theTable.ny
+    nlines += 1
+    theTable.trad = get_binary_data(fmt="%id"%theTable.nz,content=kappaContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+    
+    # Now read opacities
+    array_size = theTable.nx*theTable.ny*theTable.nz
+    array_fmt  = "%id" % array_size
+    
+    #print theTable.nx,theTable.ny,theTable.nz
+    
+    # Planck mean
+    nfloat += theTable.nz
+    nlines += 1
+    theTable.kappa_p = np.reshape(get_binary_data(fmt=array_fmt,content=kappaContent, \
+                ninteg=ninteg,nlines=nlines,nfloat=nfloat),[theTable.nx,theTable.ny,theTable.nz],order="F")
+    
+    # Rosseland mean
+    nfloat += array_size
+    nlines += 1
+    theTable.kappa_r = np.reshape(get_binary_data(fmt=array_fmt,content=kappaContent, \
+                ninteg=ninteg,nlines=nlines,nfloat=nfloat),[theTable.nx,theTable.ny,theTable.nz],order="F")
+    
+    del kappaContent
+    print("Opacity table read successfully")
+    
+    return theTable
+
+
+#===================================================================================
+# Function to interpolate simulation data onto opacity table
+#===================================================================================
+def get_opacities(holder,opacity_fname="vaytet_grey_opacities3D.bin",variables=["kappa_p","kappa_r"]):
+    
+    
+    try:
+        n = holder.opacity_table.nx
+    except AttributeError:
+        holder.opacity_table = read_opacity_table(fname=opacity_fname)
+
+    for i in range(len(variables)):
+        print("Interpolating "+variables[i])
+        grid = (holder.opacity_table.dens,holder.opacity_table.tgas,holder.opacity_table.trad)
+        func = RegularGridInterpolator(grid, getattr(holder.opacity_table,variables[i]))
+        #print 'got to here'
+        #print np.log10(holder.radiative_energy_1.values)
+        pts  = np.array([np.log10(holder.density.values),np.log10(holder.temperature.values),np.log10(holder.radiative_temperature.values)]).T
+        holder.new_field(name=variables[i],label=variables[i],values=np.power(10.0,func(pts)),verbose=False,norm=1.0)
+
+    return
+
+
+
+#===================================================================================
+#===================================================================================
+#===================================================================================
 
 #===================================================================================
 # Function to read in binary file containing resistivity table
@@ -134,7 +223,7 @@ def read_resistivity_table(fname="resnh.bin"):
     #data_fields = ["rho_eos","ener_eos","temp_eos","pres_eos","s_eos","cs_eos","xH_eos","xH2_eos","xHe_eos","xHep_eos"]
 
     # Create table container
-    theTable = ResistivityTable()
+    theTable = IsmTable()
     
     # Initialise offset counters and start reading data
     ninteg = nfloat = nlines = nstrin = nquadr = 0
@@ -158,7 +247,7 @@ def read_resistivity_table(fname="resnh.bin"):
     # Now read the bulk of the table in one go
     ninteg += ndims
     nlines += 1
-    theTable.res_chimie = np.reshape(get_binary_data(fmt="%id"%(np.prod(theTable.nx)),content=etaContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat,nstrin=nstrin,nquadr=nquadr),theTable.nx,order="F")
+    theTable.res_chimie = np.reshape(get_binary_data(fmt="%id"%(np.prod(theTable.nx)),content=etaContent,ninteg=ninteg,nlines=nlines,nfloat=nfloat),theTable.nx,order="F")
     
     print theTable.res_chimie[:,0,0]
     print np.shape(theTable.res_chimie)
