bc24bcb0d243f1a3f404cf4aee167754011a9528
Neil Vaytet <neil.vaytet@esss.se>
2019-08-19 09:38:58 +0200

    update config

--- src/osyris/config.py
+++ src/osyris/config.py
@@ -21,14 +21,7 @@ default_values = {
     "verbose"     : False,
     "path"        : ""   ,
     "variables"   : []   ,
-    "var_names"   : ["density","velocity_x","velocity_y","velocity_z","B_left_x",\
-                     "B_left_y","B_left_z","B_right_x","B_right_y","B_right_z",\
-                     "thermal_pressure","radiative_energy_1","passive_scalar_1",\
-                     "passive_scalar_2","passive_scalar_3","passive_scalar_4",\
-                     "temperature"],
-    "sink_format" : ["number","mass","dmf","x","y","z","vx","vy","vz","period",\
-                     "lx","ly","lz","acc_rate","acc_lum","age","int_lum","Teff"],
-    "colormap"    : "osyris"
+    "colormap"    : "viridis"
 }
 
 #===================================================================================
