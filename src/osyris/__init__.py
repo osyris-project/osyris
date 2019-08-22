b852f22b6404afd86152b5ae87e2125c479cdc94
Neil Vaytet <neil.vaytet@esss.se>
2019-08-22 11:20:39 +0200

    make a copy of config file into ~/osyris/config.py

--- src/osyris/__init__.py
+++ src/osyris/__init__.py
@@ -7,12 +7,33 @@ from . import ism_physics
 from .vtk import to_vtk
 from .interpolate import interpolate
 
-# Import the config from "/home/user/.osyris/config if it exists, if not load
-# the default
-from os.path import expanduser
+# Import the config from "/home/user/.osyris/config if it exists.
+# If it doesn't, try to create one by copying the default from the source.
+# If that fails, just load the default.
+from os.path import expanduser, dirname, abspath
+this_dir = dirname(abspath(__file__))
+from os import mkdir
+conf_dir = expanduser("~") + "/.osyris"
 import sys
-sys.path.append(expanduser("~") + "/.osyris")
+sys.path.append(conf_dir)
 try:
     import config
-except ModuleNotFoundError:
-    from . import config
+except ImportError:
+    from shutil import copyfile
+    mkdir(conf_dir)
+    copyfile(this_dir + "/config.py", conf_dir + "/config.py")
+    try:
+        import config
+    except ImportError:
+        from . import config
+    # try:
+    #     from shutil import copyfile
+    #     mkdir(conf_dir)
+    #     copyfile("config.py", conf_dir + "/config.py")
+    #     try:
+    #         import config
+    #     except ImportError:
+    #         from . import config
+    # except OSError:
+    #     print("imported this")
+    #     from . import config
