8eec24353b274b706c32bec57bac2ef1246642fc
Neil Vaytet <neil.vaytet@esss.se>
2019-08-22 12:00:23 +0200

    change name of config file so that its unique

--- src/osyris/__init__.py
+++ src/osyris/__init__.py
@@ -1,3 +1,24 @@
+# Import the config from "/home/user/.osyris/config if it exists.
+# If it doesn't, try to create one by copying the default from the source.
+# If that fails, just load the default.
+import os
+import sys
+conf_dir = os.path.join(os.path.expanduser("~"), ".osyris")
+sys.path.append(conf_dir)
+try:
+    import config_osyris as config
+except ImportError:
+    from shutil import copyfile
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    if not os.path.exists(conf_dir):
+        os.mkdir(conf_dir)
+    copyfile(os.path.join(this_dir, "config_osyris.py"),
+             os.path.join(conf_dir, "config_osyris.py"))
+    try:
+        import config
+    except ImportError:
+        from . import config_osyris as config
+
 from .load_ramses import RamsesData
 from .plot_histogram import plot_histogram
 from .plot_slice import plot_slice
@@ -6,34 +27,3 @@ from .plot_3d import plot_volume
 from . import ism_physics
 from .vtk import to_vtk
 from .interpolate import interpolate
-
-# Import the config from "/home/user/.osyris/config if it exists.
-# If it doesn't, try to create one by copying the default from the source.
-# If that fails, just load the default.
-from os.path import expanduser, dirname, abspath
-this_dir = dirname(abspath(__file__))
-from os import mkdir
-conf_dir = expanduser("~") + "/.osyris"
-import sys
-sys.path.append(conf_dir)
-try:
-    import config
-except ImportError:
-    from shutil import copyfile
-    mkdir(conf_dir)
-    copyfile(this_dir + "/config.py", conf_dir + "/config.py")
-    try:
-        import config
-    except ImportError:
-        from . import config
-    # try:
-    #     from shutil import copyfile
-    #     mkdir(conf_dir)
-    #     copyfile("config.py", conf_dir + "/config.py")
-    #     try:
-    #         import config
-    #     except ImportError:
-    #         from . import config
-    # except OSError:
-    #     print("imported this")
-    #     from . import config
