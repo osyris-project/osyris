bcc0e42f601325b09678e7fbbab61dc471c86328
Neil Vaytet <neil.vaytet@esss.se>
2019-08-22 13:27:16 +0200

    doc on configuration

--- docs/configuration.rst
+++ docs/configuration.rst
@@ -3,12 +3,54 @@
 Configuration
 =============
 
-Upon first import
+Upon first import, ``osyris`` will create a configuration file located in
+``/home/user/.osyris/config_osyris.py``.
+This will then be loaded by ``osyris``, and allows easy configuration for the
+default colormap, spatial scale, time unit etc.
 
 Default values
 --------------
 
-.. code-block:: sh
+- ``"nout"`` : 1 : The output number to load.
+- ``"lmax"`` : 0 : The maximum level of cells that will be loaded
+  (0 = no limit).
+- ``"center"`` : None : The center around which the grid will be centered.
+- ``"dx"`` : 0.0 : The ``x`` extent of the computational domain to be loaded.
+- ``"dy"`` : 0.0 : The ``y`` extent of the computational domain to be loaded.
+- ``"dz"`` : 0.0 : The ``z`` extent of the computational domain to be loaded.
+- ``"scale"`` : "au" : The scale for the ``x, y, z`` coordinates.
+- ``"time_unit"`` : "kyr" : The time unit (for labels, titles, etc.).
+- ``"verbose"`` : False : Print data information when loaded if ``True``.
+- ``"path"`` : "" : Path to the directory where to read outputs from,
+  if different from the current directory.
+- ``"variables"`` : [] : List of variables to be loaded.
+- ``"colormap"`` : "viridis" : Color map for slices, images, etc.
 
-   $ pip install osyris
 
+Physical constants
+------------------
+- ``"cm"`` : 1.0 : Centimeter.
+- ``"au"`` : 1.495980e+13 : Astronomical unit.
+- ``"pc"`` : 3.085678e+18 : Parsec.
+- ``"s"`` : 1.0 : Second.
+- ``"yr"`` : 365.25*86400.0 : Year.
+- ``"kyr"`` : 365.25*86400.0*1000.0 : Kilo-year.
+- ``"msun"`` : 1.9889e33 : Solar mass.
+- ``"a_r"`` : 7.56591469318689378e-015 : Radiation constant.
+- ``"c"`` : 2.9979250e+10 : Speed of light.
+
+
+Additional colormaps
+--------------------
+
+- ``"osyris"`` :
+- ``"osyris2"`` :
+- ``"osyris3"`` :
+- ``"osyris4"`` :
+- ``"osyris5"`` :
+
+Additional variables
+--------------------
+
+Define variables, using either operations on existing variables or new values,
+that will be created every time an output is loaded.
--- src/osyris/config.py
+++ src/osyris/config.py
@@ -6,9 +6,9 @@ from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 
-#===================================================================================
+#==============================================================================
 # Define default values so that you don't have to specify them every time.
-#===================================================================================
+#==============================================================================
 default_values = {
     "nout"        : 1    ,
     "lmax"        : 0    ,
@@ -24,18 +24,18 @@ default_values = {
     "colormap"    : "viridis"
 }
 
-#===================================================================================
+#==============================================================================
 # Define default units for some selected variables. The syntax is:
 # "my_variable_name" : ["combination of unit_d, unit_l, unit_t" , "string to be displayed on grid axes"]
-#===================================================================================
+#==============================================================================
 default_units = {
     ## Example for internal energy
     #"passive_scalar_4" : ["unit_d*((unit_l/unit_t)**2)" , "erg/cm3"],
 }
 
-#=======================================================================================
-# Common variables
-#=======================================================================================
+#==============================================================================
+# Physical constants
+#==============================================================================
 constants = {
     "cm"  : 1.0,
     "au"  : 1.495980e+13,
@@ -48,15 +48,19 @@ constants = {
     "c"   : 2.9979250e+10
 }
 
-#=======================================================================================
+#==============================================================================
 # Custom colormaps
-#=======================================================================================
+#==============================================================================
 cmaps = {
-    "osyris"  : ["#2b3c4e","#249593","#db6a6c","#ffffff"],
-    "osyris2" : ["#2b3c4e","#249593","#ffffff","#db6a6c","#9e4d4e"],
-    "osyris3" : ["#3d3d6b","#2a7b9b","#00baad","#57c785","#add45c","#ffc300","#ff8d1a","#ff5733","#c70039","#900c3f","#511849"],
-    "osyris4" : ["#000000","#ff5b00","#ffff00","#00ff00","#2bc184","#3d3d6b","#ffffff","#0000ff"],
-    "osyris5" : ["#ff0000","#ffff00","#00ff00","#00ffff","#0000ff","#000000","#ffffff"]
+    "osyris"  : ["#2b3c4e", "#249593", "#db6a6c", "#ffffff"],
+    "osyris2" : ["#2b3c4e", "#249593", "#ffffff", "#db6a6c", "#9e4d4e"],
+    "osyris3" : ["#3d3d6b", "#2a7b9b", "#00baad", "#57c785", "#add45c",
+                 "#ffc300", "#ff8d1a", "#ff5733", "#c70039", "#900c3f",
+                 "#511849"],
+    "osyris4" : ["#000000", "#ff5b00", "#ffff00", "#00ff00", "#2bc184",
+                 "#3d3d6b", "#ffffff", "#0000ff"],
+    "osyris5" : ["#ff0000", "#ffff00", "#00ff00", "#00ffff", "#0000ff",
+                 "#000000", "#ffffff"]
 }
 
 for key in cmaps.keys():
@@ -65,31 +69,44 @@ for key in cmaps.keys():
     plt.register_cmap(cmap=cmap)
     plt.register_cmap(cmap=cmap_r)
 
-#===================================================================================
-# Here are some additional variables that are to be computed every time data is
-# loaded.
-#===================================================================================
+
 def additional_variables(holder):
+    """
+    Here are some additional variables that are to be computed every time data
+    is loaded.
+    """
 
     # Velocity field (in case conservative variables are dumped)
-    holder.new_field(name="velocity",operation="momentum/density",unit="cm/s",label="velocity",verbose=False)
+    holder.new_field(name="velocity", operation="momentum/density",
+                     unit="cm/s", label="velocity", verbose=False)
 
     # Magnetic field
-    holder.new_field(name="B",operation="0.5*(B_left+B_right)",unit="G",label="B",verbose=False)
+    holder.new_field(name="B", operation="0.5*(B_left+B_right)", unit="G",
+                     label="B", verbose=False)
 
     # Mass
-    holder.new_field(name="mass",operation="density*((dx*"+str(constants[holder.info["scale"]])+")**3)/"+str(constants["msun"]),unit="Msun",label="Mass",verbose=False)
+    holder.new_field(name="mass",
+                     operation="density*((dx*"+str(constants[holder.info["scale"]])+")**3)/"+str(constants["msun"]),
+                     unit="Msun", label="Mass", verbose=False)
 
     # Commonly used log quantities
-    holder.new_field(name="log_rho",operation="np.log10(density)",unit="g/cm3",label="log(Density)",verbose=False)
-    holder.new_field(name="log_T",operation="np.log10(temperature)",unit="K",label="log(T)",verbose=False)
-    holder.new_field(name="log_B",operation="np.log10(B.values)",unit="G",label="log(B)",verbose=False)
-    holder.new_field(name="log_m",operation="np.log10(mass)",unit="Msun",label="log(Mass)",verbose=False)
+    holder.new_field(name="log_rho", operation="np.log10(density)",
+                     unit="g/cm3", label="log(Density)", verbose=False)
+    holder.new_field(name="log_T", operation="np.log10(temperature)", unit="K",
+                     label="log(T)", verbose=False)
+    holder.new_field(name="log_B", operation="np.log10(B.values)", unit="G",
+                     label="log(B)", verbose=False)
+    holder.new_field(name="log_m", operation="np.log10(mass)", unit="Msun",
+                     label="log(Mass)", verbose=False)
 
     # Photon density (in case conservative variables are not dumped)
     if hasattr(holder,"photon_flux_density_1"):
         for igrp in range(holder.info_rt["nGroups"]):
-            holder.new_field(name="photon_density_"+str(igrp+1),operation="photon_flux_density_"+str(igrp+1)+"/("+str(constants["c"]*holder.info_rt["rt_c_frac"])+")",unit="photons/cm3",label="photon density "+str(igrp+1),verbose=False)
+            holder.new_field(name="photon_density_"+str(igrp+1),
+                             operation="photon_flux_density_"+str(igrp+1)+"/("+str(constants["c"]*holder.info_rt["rt_c_frac"])+")",
+                             unit="photons/cm3",
+                             label="photon density "+str(igrp+1),
+                             verbose=False)
 
     #========================== ADD YOUR VARIABLES HERE ============================
 
