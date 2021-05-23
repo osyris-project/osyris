fc5fb54c515868284dbf007a3c7f01b069995392
Neil Vaytet <neil.vaytet@esss.se>
2021-05-23 16:50:55 +0200

    docs requirements

--- docs/conf.py
+++ docs/conf.py
@@ -23,16 +23,13 @@ project = 'osyris'
 copyright = '2021, Osyris contributors'
 author = 'Neil Vaytet'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.mathjax',
+    'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.mathjax',
     'nbsphinx'
 ]
 
@@ -59,18 +56,15 @@ master_doc = 'index'
 #
 html_theme = "sphinx_rtd_theme"
 
-html_theme_options = {
-    'logo_only': True
-}
+html_theme_options = {'logo_only': True}
 
 html_logo = "images/logo_osyris.png"
 html_favicon = "images/favicon.ico"
 
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 nbsphinx_timeout = 300
 
