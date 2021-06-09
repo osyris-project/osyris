1fa60b3a552f655ccaf0ee7bbd110b7472637f92
Neil Vaytet <neil.vaytet@esss.se>
2021-06-09 13:35:18 +0200

    test yapf also

---
+++ test/test_array.py
@@ -0,0 +1,12 @@
+import osyris
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def test_1d_array():
+    a =  np.arange(100.)
+    array = osyris.Array(values=a, unit="m")
+    assert array.unit == osyris.units('m')
+    assert len(array) == len(a)
+    assert array.shape == a.shape
+    assert np.allclose(array.values, a)
