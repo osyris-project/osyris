e7316eafe75a52fd6ce3cf8f7c715a5c5dcc65de
Neil Vaytet <neil.vaytet@esss.se>
2021-09-13 19:12:47 +0200

    read sink csv file

--- src/osyris/io/sinks.py
+++ src/osyris/io/sinks.py
@@ -13,8 +13,18 @@ def read_sinks(nout, path):
     sink_file = utils.generate_fname(nout, path, ftype="sink", cpuid=0, ext=".csv")
     print(sink_file)
 
-    sink_data = np.loadtxt(sink_file, delimiter=',')
-    print(sink_data)
+    sink_data = np.loadtxt(sink_file, delimiter=',', skiprows=2)
+    # print(sink_data)
+
+    with open(sink_file, 'r') as f:
+        keys = f.readline()
+        unit_combinations = f.readline()
+
+    keys = keys.lstrip(' #').rstrip('\n').split(',')
+    unit_combinations = unit_combinations.lstrip(' #').rstrip('\n').split(',')
+
+    print(keys)
+    print(unit_combinations)
 
     # self.data = Dataset()
 
