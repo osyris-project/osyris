ef32e0851b4d09a2fe49b196a7d70f2060a351d5
Adnan-Ali-Ahmad <adnan.ali.ahmad1998@gmail.com>
2022-01-09 17:18:05 +0100

    changed binary reader

--- src/osyris/core/ism_physics.py
+++ src/osyris/core/ism_physics.py
@@ -35,7 +35,7 @@ def read_opacity_table(fname):
 	ninteg = nfloat = nlines = nstrin = nquadr = 0
 
 	# Get table dimensions
-	theTable["nx"] = np.array(utils.get_binary_data(fmt="3i",content=data))
+	theTable["nx"] = np.array(utils.read_binary_data(fmt="3i",content=data))
 
 	# Read table coordinates:
 
@@ -43,17 +43,17 @@ def read_opacity_table(fname):
 	ninteg += 3
 	nfloat += 9
 	nlines += 1
-	theTable["dens"] = utils.get_binary_data(fmt="%id"%theTable["nx"][0],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+	theTable["dens"] = utils.read_binary_data(fmt="%id"%theTable["nx"][0],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
 
 	# y: gas temperature
 	nfloat += theTable["nx"][0]
 	nlines += 1
-	theTable["tgas"] = utils.get_binary_data(fmt="%id"%theTable["nx"][1],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+	theTable["tgas"] = utils.read_binary_data(fmt="%id"%theTable["nx"][1],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
 
 	# z: radiation temperature
 	nfloat += theTable["nx"][1]
 	nlines += 1
-	theTable["trad"] = utils.get_binary_data(fmt="%id"%theTable["nx"][2],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+	theTable["trad"] = utils.read_binary_data(fmt="%id"%theTable["nx"][2],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
 
 	# Now read opacities
 	array_size = np.prod(theTable["nx"])
@@ -64,13 +64,13 @@ def read_opacity_table(fname):
 	# Planck mean
 	nfloat += theTable["nx"][2]
 	nlines += 1
-	theTable.kappa_p = np.reshape(utils.get_binary_data(fmt=array_fmt,content=data, \
+	theTable.kappa_p = np.reshape(utils.read_binary_data(fmt=array_fmt,content=data, \
 	            ninteg=ninteg,nlines=nlines,nfloat=nfloat),theTable["nx"],order="F")
 
 	# Rosseland mean
 	nfloat += array_size
 	nlines += 1
-	theTable["kappa_r"] = np.reshape(utils.get_binary_data(fmt=array_fmt,content=data, \
+	theTable["kappa_r"] = np.reshape(utils.read_binary_data(fmt=array_fmt,content=data, \
 	            ninteg=ninteg,nlines=nlines,nfloat=nfloat),theTable["nx"],order="F")
 
 	del data
@@ -83,21 +83,21 @@ def read_opacity_table(fname):
 	return theTable
 """
 def get_opacities(holder,fname="vaytet_grey_opacities3D.bin",variables=["kappa_p","kappa_r"]):
-    
-    try:
-        n = holder.opacity_table.nx
-    except AttributeError:
-        holder.opacity_table = read_opacity_table(fname=fname)
-    
-    if not hasattr(holder,"radiative_temperature"):
-        print("Radiative temperature is not defined. Computing it now.")
-        holder.new_field(name="radiative_temperature",operation="(radiative_energy_1/"+str(conf.constants["a_r"])+")**0.25",label="Trad")
 
-    pts = np.array([np.log10(holder.density.values),np.log10(holder.temperature.values),np.log10(holder.radiative_temperature.values)]).T
-    for var in variables:
-        print("Interpolating "+var)
-        vals = ism_interpolate(holder.opacity_table,getattr(holder.opacity_table,var),pts)
-        holder.new_field(name=var,label=var,values=vals,verbose=False,unit="cm2/g")
+	try:
+	    n = holder.opacity_table.nx
+	except AttributeError:
+	    holder.opacity_table = read_opacity_table(fname=fname)
 
-    return
+	if not hasattr(holder,"radiative_temperature"):
+	    print("Radiative temperature is not defined. Computing it now.")
+	    holder.new_field(name="radiative_temperature",operation="(radiative_energy_1/"+str(conf.constants["a_r"])+")**0.25",label="Trad")
+
+	pts = np.array([np.log10(holder.density.values),np.log10(holder.temperature.values),np.log10(holder.radiative_temperature.values)]).T
+	for var in variables:
+	    print("Interpolating "+var)
+	    vals = ism_interpolate(holder.opacity_table,getattr(holder.opacity_table,var),pts)
+	    holder.new_field(name=var,label=var,values=vals,verbose=False,unit="cm2/g")
+
+	return
 """
