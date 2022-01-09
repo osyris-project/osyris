30fcd63883d36b9d25ec2f8d3b1d679c054e3398
Adnan-Ali-Ahmad <adnan.ali.ahmad1998@gmail.com>
2022-01-09 17:13:42 +0100

    fixed indentation error

--- src/osyris/core/ism_physics.py
+++ src/osyris/core/ism_physics.py
@@ -29,58 +29,58 @@ def read_opacity_table(fname):
 		data = f.read()
 
 	# Create table container
-    theTable = dict()
-    
-    # Initialise offset counters and start reading data
-    ninteg = nfloat = nlines = nstrin = nquadr = 0
-    
-    # Get table dimensions
-    theTable["nx"] = np.array(utils.get_binary_data(fmt="3i",content=data))
-    
-    # Read table coordinates:
-    
-    # x: density
-    ninteg += 3
-    nfloat += 9
-    nlines += 1
-    theTable["dens"] = utils.get_binary_data(fmt="%id"%theTable["nx"][0],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
-    
-    # y: gas temperature
-    nfloat += theTable["nx"][0]
-    nlines += 1
-    theTable["tgas"] = utils.get_binary_data(fmt="%id"%theTable["nx"][1],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
-    
-    # z: radiation temperature
-    nfloat += theTable["nx"][1]
-    nlines += 1
-    theTable["trad"] = utils.get_binary_data(fmt="%id"%theTable["nx"][2],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
-    
-    # Now read opacities
-    array_size = np.prod(theTable["nx"])
-    array_fmt  = "%id" % array_size
-    
-    #print theTable.nx,theTable.ny,theTable.nz
-    
-    # Planck mean
-    nfloat += theTable["nx"][2]
-    nlines += 1
-    theTable.kappa_p = np.reshape(utils.get_binary_data(fmt=array_fmt,content=data, \
-                ninteg=ninteg,nlines=nlines,nfloat=nfloat),theTable["nx"],order="F")
-    
-    # Rosseland mean
-    nfloat += array_size
-    nlines += 1
-    theTable["kappa_r"] = np.reshape(utils.get_binary_data(fmt=array_fmt,content=data, \
-                ninteg=ninteg,nlines=nlines,nfloat=nfloat),theTable["nx"],order="F")
-    
-    del data
+	theTable = dict()
 
-    theTable["grid"] = (theTable["dens"],theTable["tgas"],theTable["trad"])
-    
-    
-    print("Opacity table read successfully")
-    
-    return theTable
+	# Initialise offset counters and start reading data
+	ninteg = nfloat = nlines = nstrin = nquadr = 0
+
+	# Get table dimensions
+	theTable["nx"] = np.array(utils.get_binary_data(fmt="3i",content=data))
+
+	# Read table coordinates:
+
+	# x: density
+	ninteg += 3
+	nfloat += 9
+	nlines += 1
+	theTable["dens"] = utils.get_binary_data(fmt="%id"%theTable["nx"][0],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+
+	# y: gas temperature
+	nfloat += theTable["nx"][0]
+	nlines += 1
+	theTable["tgas"] = utils.get_binary_data(fmt="%id"%theTable["nx"][1],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+
+	# z: radiation temperature
+	nfloat += theTable["nx"][1]
+	nlines += 1
+	theTable["trad"] = utils.get_binary_data(fmt="%id"%theTable["nx"][2],content=data,ninteg=ninteg,nlines=nlines,nfloat=nfloat)
+
+	# Now read opacities
+	array_size = np.prod(theTable["nx"])
+	array_fmt  = "%id" % array_size
+
+	#print theTable.nx,theTable.ny,theTable.nz
+
+	# Planck mean
+	nfloat += theTable["nx"][2]
+	nlines += 1
+	theTable.kappa_p = np.reshape(utils.get_binary_data(fmt=array_fmt,content=data, \
+	            ninteg=ninteg,nlines=nlines,nfloat=nfloat),theTable["nx"],order="F")
+
+	# Rosseland mean
+	nfloat += array_size
+	nlines += 1
+	theTable["kappa_r"] = np.reshape(utils.get_binary_data(fmt=array_fmt,content=data, \
+	            ninteg=ninteg,nlines=nlines,nfloat=nfloat),theTable["nx"],order="F")
+
+	del data
+
+	theTable["grid"] = (theTable["dens"],theTable["tgas"],theTable["trad"])
+
+
+	print("Opacity table read successfully")
+
+	return theTable
 """
 def get_opacities(holder,fname="vaytet_grey_opacities3D.bin",variables=["kappa_p","kappa_r"]):
     
