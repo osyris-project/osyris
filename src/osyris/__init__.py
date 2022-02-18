90c6381b7da179dcc89bd9318fa3109f87ec33c5
Adnan-Ali-Ahmad <adnan.ali.ahmad1998@gmail.com>
2022-02-18 22:11:23 +0100

    forgot to remove spatial import

--- src/osyris/__init__.py
+++ src/osyris/__init__.py
@@ -6,4 +6,3 @@
 from .config import config, units
 from .plot import histogram1d, histogram2d, plane, scatter, map, plot
 from .core import Array, Datagroup, Dataset, Plot
-from .spatial import spatial
