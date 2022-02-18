bf66a73dc4816f400f3e1469336484347b8210f2
Adnan-Ali-Ahmad <adnan.ali.ahmad1998@gmail.com>
2022-02-18 22:23:17 +0100

    spatial PR commit

--- src/osyris/__init__.py
+++ src/osyris/__init__.py
@@ -6,3 +6,4 @@
 from .config import config, units
 from .plot import histogram1d, histogram2d, plane, scatter, map, plot
 from .core import Array, Datagroup, Dataset, Plot
+from .spatial import spatial
