aee69f17d812e482e2c2f1d38e5cee260a2b0712
Neil Vaytet <neil.vaytet@esss.se>
2022-02-19 21:50:05 +0100

    flake8

--- test/test_datagroup.py
+++ test/test_datagroup.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Osyris contributors (https://github.com/osyris-project/osyris)
 from osyris import Array, Datagroup
-import numpy as np
 import pytest
 
 
