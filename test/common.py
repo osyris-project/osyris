6a024bfc5492f485c0173b54a06594bd1a66768d
Neil Vaytet <neil.vaytet@esss.se>
2022-04-25 21:01:11 +0200

    add common

---
+++ test/common.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Osyris contributors (https://github.com/osyris-project/osyris)
+
+import numpy as np
+
+
+def allclose(a, b):
+    if a.unit != b.unit:
+        return False
+    return np.allclose(a.values, b.values)
