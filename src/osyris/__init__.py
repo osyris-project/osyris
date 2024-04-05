3a960e55b59d6b2e9bf173fe9febad2f9752d0dd
Neil Vaytet <neil.vaytet@ess.eu>
2024-04-05 14:10:03 +0200

    add __version__ to init

--- src/osyris/__init__.py
+++ src/osyris/__init__.py
@@ -1,10 +1,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Osyris contributors (https://github.com/osyris-project/osyris)
 
-# flake8: noqa
+import importlib.metadata
 
 from .config import config
 from .units import units
 from .core import Array, Datagroup, Dataset, Plot, Vector
 from .plot import histogram1d, histogram2d, scatter, map, plot
 from .spatial import extract_box, extract_sphere
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+del importlib
