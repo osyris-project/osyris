98c4670170baad7ce24f3b86e6c4b5a8b77a6a00
Neil Vaytet <neil.vaytet@esss.se>
2024-05-24 17:48:34 +0200

    isort

--- src/osyris/io/__init__.py
+++ src/osyris/io/__init__.py
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Osyris contributors (https://github.com/osyris-project/osyris)
 
+from . import utils
 from .loader import Loader
 from .ramses import RamsesDataset
-from . import utils
 
 __all__ = ["Loader", "RamsesDataset", "utils"]
--- src/osyris/io/ramses.py
+++ src/osyris/io/ramses.py
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Osyris contributors (https://github.com/osyris-project/osyris)
-from .loader import Loader
 from .. import config
 from ..core import Dataset
+from .loader import Loader
 
 
 class RamsesDataset(Dataset):
